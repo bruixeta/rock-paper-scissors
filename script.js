dcf46f3b17a8777f6213755b13a46f298ab5ce60
Sofia <150052649+bruixeta@users.noreply.github.com>
2024-01-31 13:09:42 +0100

    Create Function playRound
    The function contains a set of rules to determine what an element
    wins to and loses to. Then it uses that set to determine who the
    winner of the round is.

--- script.js
+++ script.js
@@ -6,6 +6,31 @@ Array.prototype.sample = function(){
   }
 
 function getComputerChoice(){
-    let computerChoice = ["Rock", "Paper", "Scissors"].sample();
+    const computerChoice = ["rock", "paper", "scissors"].sample();
     return computerChoice;
 }
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    let winner = "Error";
+
+    const rules = {
+        rock: { winsTo: "scissors", losesTo: "paper"},
+        paper:  { winsTo: "rock", losesTo: "scissors"},
+        scissors:  { winsTo: "paper", losesTo: "rock"}
+    }
+
+    if (playerSelection===computerSelection){
+        winner = "Tie";
+    }
+    else if (rules[playerSelection].winsTo === computerSelection){
+        winner = "Player";
+    }
+    else winner = "Computer";
+
+    return winner;
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
