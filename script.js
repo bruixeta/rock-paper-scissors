c182bbac9253a8906fd74c83fcf7d9aa3c5407a6
Sofia <150052649+bruixeta@users.noreply.github.com>
2024-01-31 13:46:10 +0100

    Create playGame function
    Includes a loop that plays 5 rounds and keeps the
    score, to determine the game winner.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //This is a function that applies to arrays, and creates the 
 //method [].sample(). When applied, it returns a 
 //random element from the array
+
 Array.prototype.sample = function(){
     return this[Math.floor(Math.random()*this.length)];
   }
@@ -12,7 +13,8 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
-    let winner = "Error";
+    let winner;
+    //let winnerDeclaration = "Player chose " + playerSelection + " and Computer chose " + computerSelection + ".";
 
     const rules = {
         rock: { winsTo: "scissors", losesTo: "paper"},
@@ -22,15 +24,47 @@ function playRound(playerSelection, computerSelection){
 
     if (playerSelection===computerSelection){
         winner = "Tie";
+        //winnerDeclaration = winnerDeclaration + " It is a Tie!"
     }
     else if (rules[playerSelection].winsTo === computerSelection){
         winner = "Player";
+        //winnerDeclaration = winnerDeclaration + "You won!" + playerSelection + " beats " + computerSelection + ".";
     }
-    else winner = "Computer";
-
+    else {
+        winner = "Computer";
+        //winnerDeclaration = winnerDeclaration + " You lost! " + computerSelection + " beats " + playerSelection + ".";
+    }
+    //console.log(winnerDeclaration);
     return winner;
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+function playGame(){
+    score = [0,0];
+    let gameWinner;
+    for (let i = 1; i < 6; i++) {
+        let playerSelection = prompt("choose rock paper or scissors");
+        let computerSelection = getComputerChoice();
+        winner = playRound(playerSelection, computerSelection);
+
+        if(winner==="Player") score[0] += 1;
+        else if (winner ==="Computer") score [1] += 1;
+
+        console.log("Player chose " + playerSelection + " and Computer chose " + computerSelection + ".")
+        console.log("The winner of Round " + i + " is " + winner + ".")
+        console.log("The current score is: Player(" + score[0] + ") vs. Computer(" + score[1] + ").")
+    }
+    if(score[0]>score[1]){
+        gameWinner = "Player";
+    }
+    else if(score[1]>score[0]){
+        gameWinner = "Computer";
+    }
+
+    console.log(gameWinner + " won the game! The final score was: Player(" + score[0] + ") vs. Computer(" + score[1] + ").")
+}
+
+
+
+
+
 
