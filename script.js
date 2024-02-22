7196c7922415c500974bb5c21930b85695233d3f
Sofia <150052649+bruixeta@users.noreply.github.com>
2024-02-22 13:11:07 +0100

    Create player buttons
    Add an event listener to the buttons that call the playRound
    function with the correct playerSelection every time a button is
    clicked. (keeping the console.log)

--- script.js
+++ script.js
@@ -2,19 +2,36 @@
 //method [].sample(). When applied, it returns a 
 //random element from the array
 
+
+
 Array.prototype.sample = function(){
     return this[Math.floor(Math.random()*this.length)];
   }
 
-function getComputerChoice(){
-    const computerChoice = ["rock", "paper", "scissors"].sample();
-    return computerChoice;
+function getComputerSelection(){
+    const computerSelection = ["rock", "paper", "scissors"].sample();
+    return computerSelection;
+}
+
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playGame(button.id);
+    });
+});
+
+
+function playGame(playerSelection){
+    let computerSelection = getComputerSelection();
+    const winner = playRound(playerSelection, computerSelection);
+    console.log(winner);
+    return winner;
 }
 
 function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
     let winner;
-    //let winnerDeclaration = "Player chose " + playerSelection + " and Computer chose " + computerSelection + ".";
 
     const rules = {
         rock: { winsTo: "scissors", losesTo: "paper"},
@@ -24,47 +41,59 @@ function playRound(playerSelection, computerSelection){
 
     if (playerSelection===computerSelection){
         winner = "Tie";
-        //winnerDeclaration = winnerDeclaration + " It is a Tie!"
     }
     else if (rules[playerSelection].winsTo === computerSelection){
         winner = "Player";
-        //winnerDeclaration = winnerDeclaration + "You won!" + playerSelection + " beats " + computerSelection + ".";
     }
     else {
         winner = "Computer";
-        //winnerDeclaration = winnerDeclaration + " You lost! " + computerSelection + " beats " + playerSelection + ".";
     }
-    //console.log(winnerDeclaration);
     return winner;
 }
+
+
+
+
+
+
+
+
+/*
 
 function playGame(){
     score = [0,0];
     let gameWinner;
-    for (let i = 1; i < 6; i++) {
-        let playerSelection = prompt("choose rock paper or scissors");
+    let playerSelection;
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            playerSelection = button.id;
+        });
+    });
+    
+    //for (let i = 1; i < 6; i++) {
+        //let playerSelection = prompt("choose rock paper or scissors");
         let computerSelection = getComputerChoice();
         winner = playRound(playerSelection, computerSelection);
-
+                
         if(winner==="Player") score[0] += 1;
         else if (winner ==="Computer") score [1] += 1;
 
         console.log("Player chose " + playerSelection + " and Computer chose " + computerSelection + ".")
         console.log("The winner of Round " + i + " is " + winner + ".")
         console.log("The current score is: Player(" + score[0] + ") vs. Computer(" + score[1] + ").")
-    }
-    if(score[0]>score[1]){
-        gameWinner = "Player";
-    }
-    else if(score[1]>score[0]){
-        gameWinner = "Computer";
-    }
+    //}
+    //if(score[0]>score[1]){
+       // gameWinner = "Player";
+    //}
+    //else if(score[1]>score[0]){
+     //   gameWinner = "Computer";
+    //}
 
     console.log(gameWinner + " won the game! The final score was: Player(" + score[0] + ") vs. Computer(" + score[1] + ").")
 }
 
 
-
+*/
 
 
 
